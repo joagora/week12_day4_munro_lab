38b02886fc831f5109100e6d48ae9deb042d45a2
Joanna Gora <joanna.gora@protonmail.com>
2018-09-13 14:16:41 +0100

    refactor to fetch

--- src/models/munros.js
+++ src/models/munros.js
@@ -3,20 +3,19 @@ const PubSub = require('../helpers/pub_sub.js');
 const RequestHelper = require('../helpers/request_helper.js');
 
 const Munros = function () {
-  this.munrosNames = [];
+  this.data = null;
 }
 
 Munros.prototype.getData = function() {
   const requestHelper = new RequestHelper('https://munroapi.herokuapp.com/api/munros');
-  requestHelper.get((data) => {
-    // this.handleDataReady(data);
-    PubSub.publish('Munros:munros-names-ready', data);
-  })
+  requestHelper.get()
+    .then((data) => {
+      this.data = data;
+      PubSub.publish('Munros:munros-names-ready', this.data);
+    })
+    .catch((message) => {
+      console.error(message);
+    })
 }
-
-// Munros.prototype.handleDataReady = function(munrosData) {
-//   const munros = munrosData.map(munro => munro.name);
-//   this.munrosNames = munrosNames;
-// }
 
 module.exports = Munros;
