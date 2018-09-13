7f31a8ba8a252b6ffb3599bd63eb51f49bb0251e
Joanna Gora <joanna.gora@protonmail.com>
2018-09-13 14:01:01 +0100

    populate munro details - mvp complete

--- src/views/munros_details_view.js
+++ src/views/munros_details_view.js
@@ -10,6 +10,8 @@ MunrosDetailsView.prototype.render = function() {
   const munroName = this.createMunroHeading();
   munroDetails.appendChild(munroName);
   this.munroListContainer.appendChild(munroDetails);
+  const munroUl = this.createMunroUl();
+  this.munroListContainer.appendChild(munroUl);
 }
 
 MunrosDetailsView.prototype.createMunroHeading = function() {
@@ -18,5 +20,19 @@ MunrosDetailsView.prototype.createMunroHeading = function() {
   return nameHeading;
 }
 
+MunrosDetailsView.prototype.createMunroUl = function() {
+  const munroDetailsUl = document.createElement('ul');
+  this.populateUl(munroDetailsUl);
+  return munroDetailsUl;
+}
+
+MunrosDetailsView.prototype.populateUl = function(ul) {
+  const heightLi = document.createElement('li');
+  heightLi.textContent = this.munro.height;
+  const meaningLi = document.createElement('li');
+  meaningLi.textContent = this.munro.meaning;
+  ul.appendChild(heightLi);
+  ul.appendChild(meaningLi);
+}
 
 module.exports = MunrosDetailsView;
